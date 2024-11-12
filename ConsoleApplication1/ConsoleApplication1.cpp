5229dad3afcdef3527d73ba67d202ad5654a1e9e
0Kapt0 <lorenzo.capitaine@gmail.com>
2024-11-12 17:59:15 +0100

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,20 +4,20 @@
 #include <ctime>
 #include <Windows.h>
 
-// Dimensions de la carte
+//Dimensions de la carte
 const int LARGEUR_MAP = 20;
 const int HAUTEUR_MAP = 20;
 
-// Définition des types de terrain
+//Définition des types de terrain
 const char VIDE = '.';
 const char TREFLE = 'T';
 const char EAU = 'W';
 
-// Les nombres des animaux
-int NA = 0;  // Nombre d'aigles
-int NR = 0;  // Nombre de renards
-int NL = 0;  // Nombre de lapins
-int NT = 0;  // Nombre de trèfles
+//Les nombres des animaux
+int NA = 0;  //Nombre d'aigles
+int NR = 0;  //Nombre de renards
+int NL = 0;  //Nombre de lapins
+int NT = 0;  //Nombre de trèfles
 
 const int COLOR_GREEN = 2;
 const int COLOR_WHITE = 7;
@@ -25,12 +25,12 @@ const int COLOR_RED = 4;
 const int COLOR_YELLOW = 6;
 const int COLOR_BLUE = 1;
 
-// Fonction pour définir la couleur de la console
+//Fonction pour définir la couleur de la console
 void setConsoleColor(int couleur) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), couleur);
 }
 
-// Les classes des animaux
+//Les classes des animaux
 class Animal {
 public:
     int nombre;
@@ -38,7 +38,7 @@ public:
     virtual void Deplacement() = 0;
 };
 
-// Structure représentant une cellule de la carte
+//Structure représentant une cellule de la carte
 struct Cell {
     bool vivant = true;
     int ressource = 0;
@@ -99,23 +99,22 @@ public:
     Trefle(int n) : Animal(n) {}
 };
 
-// Classe représentant la carte
 class Carte {
 public:
     std::vector<std::vector<Cell>> map;  // Carte de type Cell
 
-    // Constructeur de la carte
+    //Construit de la carte
     Carte() {
         map.resize(HAUTEUR_MAP, std::vector<Cell>(LARGEUR_MAP));
         remplirMap();
     }
 
-    // Remplir la carte avec des ressources et des points d'eau
+    //Rempli la carte avec des ressources et des points d'eau
     void remplirMap() {
         int nbEau = 0;  // Nombre de points d'eau
         int nbRessources = 0;  // Nombre de ressources
 
-        // Placer les points d'eau (maximum 5)
+        //Place les points d'eau (maximum 5)
         while (nbEau < 3) {
             int x = rand() % LARGEUR_MAP;
             int y = rand() % HAUTEUR_MAP;
@@ -126,7 +125,7 @@ public:
             }
         }
 
-        // Placer les ressources (maximum 10)
+        //Place les ressources (maximum 10)
         while (nbRessources < 10) {
             int x = rand() % LARGEUR_MAP;
             int y = rand() % HAUTEUR_MAP;
@@ -138,9 +137,9 @@ public:
         }
     }
 
-    // Placer des animaux sur la carte
+    //Place des animaux sur la carte
     void placerAnimaux(int nombreAigles, int nombreRenards, int nombreLapins) {
-        // Placer les aigles
+        //Place les aigles
         for (int i = 0; i < nombreAigles; ++i) {
             int x = rand() % LARGEUR_MAP;
             int y = rand() % HAUTEUR_MAP;
@@ -150,7 +149,7 @@ public:
             }
         }
 
-        // Placer les renards
+        //Place les renards
         for (int i = 0; i < nombreRenards; ++i) {
             int x = rand() % LARGEUR_MAP;
             int y = rand() % HAUTEUR_MAP;
@@ -160,7 +159,7 @@ public:
             }
         }
 
-        // Placer les lapins
+        //Place les lapins
         for (int i = 0; i < nombreLapins; ++i) {
             int x = rand() % LARGEUR_MAP;
             int y = rand() % HAUTEUR_MAP;
@@ -171,7 +170,7 @@ public:
         }
     }
 
-    // Afficher la carte
+    //Affiche la carte
     void afficherCarte() const {
         for (int x = 0; x < LARGEUR_MAP; ++x) {
             std::cout << " ---";
@@ -179,8 +178,8 @@ public:
         std::cout << " " << std::endl;
         for (int y = 0; y < HAUTEUR_MAP; ++y) {
             for (int x = 0; x < LARGEUR_MAP; ++x) {
-                setConsoleColor(map[y][x].couleur); // Appliquer la couleur de la cellule
-                std::cout << "| " << map[y][x].animal << " "; // Afficher l'animal avec la couleur
+                setConsoleColor(map[y][x].couleur); //Applique la couleur de la cellule
+                std::cout << "| " << map[y][x].animal << " "; //Affiche l'animal avec la couleur
             }
             std::cout << "|" << std::endl;
             for (int x = 0; x < LARGEUR_MAP; ++x) {
@@ -192,18 +191,18 @@ public:
 };
 
 int main() {
-    srand(time(0));  // Initialisation du générateur de nombres aléatoires
+    srand(time(0));  //Initialise le générateur de nombres aléatoires
 
-    // Créer la carte
+    //Créer la carte
     Carte carte;
 
-    // Remplir la carte avec des ressources et des points d'eau
+    //Rempli la carte avec des ressources et des points d'eau
     carte.remplirMap();
 
-    // Placer des animaux (aigles, renards, lapins)
+    //Place des animaux (aigles, renards, lapins)
     carte.placerAnimaux(2, 8, 18);
 
-    // Afficher la carte
+    //Affiche la carte
     std::cout << "Bienvenue dans le simulateur ecosysteme\n";
     carte.afficherCarte();
 
