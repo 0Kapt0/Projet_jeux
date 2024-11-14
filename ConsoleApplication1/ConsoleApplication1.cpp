e60e64c3bf9db41357427c468927c53d2c2b4dc4
Lord0k <lanfeust91@gmail.com>
2024-11-14 15:16:46 +0100

    Update ConsoleApplication1.cpp
    reset mentale

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,240 +1,242 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <Windows.h>
+using namespace std;
 
-//Dimensions de la carte qqqqqq
-const int LARGEUR_MAP = 20;
-const int HAUTEUR_MAP = 20;
-
-//Définition des types de terrain
-const char VIDE = '.';
-const char TREFLE = 'T';
-const char EAU = 'W';
-
-//Les nombres des animaux
-int NA = 0;  //Nombre d'aigles
-int NR = 0;  //Nombre de renards
-int NL = 0;  //Nombre de lapins
-int NT = 0;  //Nombre de trèfles
-
-const int COLOR_GREEN = 2;
-const int COLOR_WHITE = 7;
-const int COLOR_RED = 4;
-const int COLOR_YELLOW = 6;
-const int COLOR_BLUE = 1;
-
-//Fonction pour définir la couleur de la console
-void setConsoleColor(int couleur) {
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), couleur);
-}
-
-struct Position {
-    int x, y;
-    Position(int _x, int _y) : x(_x), y(_y) {}
-};
-
-//Structure représentant une cellule de la carte
-struct Cell {
-    bool vivant = true;
-    int ressource = 0;
-    int eau = 0;
-    int quete = 0;
-    int deplacement = 0;
-    char animal = ' ';
-    int couleur = COLOR_WHITE;
-};
-
-class Carte {
+class Eau {
 public:
-    std::vector<std::vector<Cell>> map;  // Carte de type Cell
+    static int quantite;
 
-    //Construit de la carte
-    Carte() {
-        map.resize(HAUTEUR_MAP, std::vector<Cell>(LARGEUR_MAP));
-        remplirMap();
+    static void produire() {
+        quantite += 10;
     }
 
-    //Rempli la carte avec des ressources et des points d'eau
-    void remplirMap() {
-        int nbEau = 0;  // Nombre de points d'eau
-        int nbRessources = 0;  // Nombre de ressources
-
-        //Place les points d'eau (maximum 5)
-        while (nbEau < 3) {
-            int x = rand() % LARGEUR_MAP;
-            int y = rand() % HAUTEUR_MAP;
-            if (map[y][x].animal == ' ') {
-                map[y][x].animal = EAU;
-                map[y][x].couleur = COLOR_BLUE; // Bleu pour l'eau
-                nbEau++;
-            }
+    static bool boire(int quantiteBoire) {
+        if (quantite >= quantiteBoire) {
+            quantite -= quantiteBoire;
+            return true;
         }
-
-        //Place les ressources (maximum 10)
-        while (nbRessources < 10) {
-            int x = rand() % LARGEUR_MAP;
-            int y = rand() % HAUTEUR_MAP;
-            if (map[y][x].animal == ' ') {
-                map[y][x].animal = TREFLE;
-                map[y][x].couleur = COLOR_WHITE; // Vert pour le trèfle
-                nbRessources++;
-            }
-        }
-    }
-    
-    //Verifi si une position est libre
-    bool estlibre(int x, int y) {
-        return x >= 0 && x < LARGEUR_MAP && y >= 0 && y < HAUTEUR_MAP && map[y][x].animal == ' ';
-    }
-    
-    //Met à jour la position d'un animal
-    void mettreAJourPosition(Position& anciennePos, Position nouvellePos, char symboleAnimal, int couleurAnimal) {
-        map[anciennePos.y][anciennePos.x].animal = VIDE; //Efface ancienne position
-        map[nouvellePos.y][nouvellePos.x].animal = symboleAnimal; //Place l'animal à la nouvelle position
-        map[nouvellePos.y][nouvellePos.x].couleur = couleurAnimal;
-        anciennePos = nouvellePos;
-    }
-
-
-    //Affiche la carte
-    void afficherCarte() const {
-        system("cls"); // Efface l'écran
-        for (int x = 0; x < LARGEUR_MAP; ++x) {
-            std::cout << " ---";
-        }
-        std::cout << " " << std::endl;
-        for (int y = 0; y < HAUTEUR_MAP; ++y) {
-            for (int x = 0; x < LARGEUR_MAP; ++x) {
-                setConsoleColor(map[y][x].couleur); //Applique la couleur de la cellule
-                std::cout << "| " << map[y][x].animal << " "; //Affiche l'animal avec la couleur
-            }
-            std::cout << "|" << std::endl;
-            for (int x = 0; x < LARGEUR_MAP; ++x) {
-                std::cout << " ---";
-            }
-            std::cout << " " << std::endl;
-        }
-        setConsoleColor(COLOR_WHITE); // Réinitialise la couleur
+        return false;
     }
 };
+int Eau::quantite = 100;
 
-//Les classes des animaux
-class Animal {
-protected:
-    Position pos;
-    Carte* carte;
-    char symbole;
-    int couleur;
-
+class Trefle {
 public:
-    Animal(Carte* _carte, char _symbole, int _couleur)
-        : carte(_carte), symbole(_symbole), couleur(_couleur) {
-        placerAleatoirement();
+    static int quantite;
+
+    static void produire() {
+        quantite += 5;
     }
 
-    virtual void seDeplacer() = 0;
-
-    void placerAleatoirement() {
-        int x, y;
-        do {
-            x = rand() % LARGEUR_MAP;
-            y = rand() % HAUTEUR_MAP;
-        } while (!carte->estlibre(x, y));
-        pos = Position(x, y);
-        carte->map[y][x].animal = symbole;
-        carte->map[y][x].couleur = couleur;
-    }
-};
-
-class Aigle : public Animal {
-public:
-    Aigle(Carte* carte) : Animal(carte, 'A', COLOR_RED) {}
-
-    void seDeplacer() override {
-        int dx = (rand() % 7) - 3;
-        int dy = (rand() % 7) - 3;
-        Position nouvellePos(pos.x + dx, pos.y + dy);
-
-        if (carte->estlibre(nouvellePos.x, nouvellePos.y)) {
-            carte->mettreAJourPosition(pos, nouvellePos, symbole, couleur);
+    static bool manger(int quantiteManger) {
+        if (quantite >= quantiteManger) {
+            quantite -= quantiteManger;
+            return true;
         }
+        return false;
     }
 };
+int Trefle::quantite = 100;
 
-class Renard : public Animal {
+class Lapin {
 public:
-    Renard(Carte* carte) : Animal(carte, 'R', COLOR_YELLOW) {}
+    static int population;
+    static int totalManges;
+    static int reproductions;
 
-    void seDeplacer() override {
-        int dx = (rand() % 5) - 2;
-        int dy = (rand() % 5) - 2;
-        Position nouvellePos(pos.x + dx, pos.y + dy);
+    int treflesManges = 0;
+    int eauBue = 0;
 
-        if (carte->estlibre(nouvellePos.x, nouvellePos.y)) {
-            carte->mettreAJourPosition(pos, nouvellePos, symbole, couleur);
-        }
-    }
-};
-
-class Lapin : public Animal {
-public:
-    Lapin(Carte* carte) : Animal(carte, 'L', COLOR_GREEN) {}
-
-    void seDeplacer() override {
-        int dx = (rand() % 3) - 1;
-        int dy = (rand() % 3) - 1;
-        Position nouvellePos(pos.x + dx, pos.y + dy);
-
-        if (carte->estlibre(nouvellePos.x, nouvellePos.y)) {
-            carte->mettreAJourPosition(pos, nouvellePos, symbole, couleur);
-        }
-    }
-};
-
-// Classe Jeu
-class Jeu {
-private:
-    Carte carte;
-    std::vector<Animal*> animaux;
-
-public:
-    Jeu() {
-        // Crée les animaux et les ajoute à la liste
-        for (int i = 0; i < 2; ++i) animaux.push_back(new Aigle(&carte));
-        for (int i = 0; i < 6; ++i) animaux.push_back(new Renard(&carte));
-        for (int i = 0; i < 18; ++i) animaux.push_back(new Lapin(&carte));
+    void boire() {
+        if (Eau::boire(4)) eauBue += 4;
     }
 
-    ~Jeu() {
-        // Libère la mémoire allouée pour chaque animal
-        for (auto& animal : animaux) delete animal;
+    void manger() {
+        if (Trefle::manger(4)) treflesManges += 4;
     }
 
-    void demarrer() {
-        char choix;
-        do {
-            carte.afficherCarte(); // Affiche la carte
+    bool peutSeReproduire() const {
+        return treflesManges >= 10 && eauBue >= 10;
+    }
 
-            std::cout << "Voulez-vous continuer ? (o/n): ";
-            std::cin >> choix;
+    void resetReproduction() {
+        treflesManges = 0;
+        eauBue = 0;
+    }
 
-            if (choix == 'o') {
-                // Déplacer chaque animal
-                for (auto& animal : animaux) {
-                    animal->seDeplacer();
+    static void reproduire(vector<Lapin>& lapins) {
+        for (size_t i = 0; i < lapins.size(); ++i) {
+            if (lapins[i].peutSeReproduire()) {
+                for (size_t j = i + 1; j < lapins.size(); ++j) {
+                    if (lapins[j].peutSeReproduire()) {
+                        lapins[i].resetReproduction();
+                        lapins[j].resetReproduction();
+                        lapins.push_back(Lapin());
+                        ++population;
+                        ++reproductions;
+                        break;
+                    }
                 }
             }
-        } while (choix == 'o');
+        }
     }
 };
+int Lapin::population = 18;
+int Lapin::totalManges = 0;
+int Lapin::reproductions = 0;
+
+class Renard {
+public:
+    static int population;
+    static int totalManges;
+    static int reproductions;
+
+    int lapinsManges = 0;
+    int eauBue = 0;
+
+    void boire() {
+        if (Eau::boire(4)) eauBue += 4;
+    }
+
+    void manger(vector<Lapin>& lapins) {
+        if (lapins.size() >= 4) {
+            lapins.erase(lapins.end() - 4, lapins.end());
+            lapinsManges += 4;
+            Lapin::population -= 4;
+            Lapin::totalManges += 4;
+        }
+    }
+
+    bool peutSeReproduire() const {
+        return lapinsManges >= 20 && eauBue >= 20;
+    }
+
+    void resetReproduction() {
+        lapinsManges = 0;
+        eauBue = 0;
+    }
+
+    static void reproduire(vector<Renard>& renards) {
+        for (size_t i = 0; i < renards.size(); ++i) {
+            if (renards[i].peutSeReproduire()) {
+                for (size_t j = i + 1; j < renards.size(); ++j) {
+                    if (renards[j].peutSeReproduire()) {
+                        renards[i].resetReproduction();
+                        renards[j].resetReproduction();
+                        renards.push_back(Renard());
+                        ++population;
+                        ++reproductions;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+};
+int Renard::population = 8;
+int Renard::totalManges = 0;
+int Renard::reproductions = 0;
+
+class Aigle {
+public:
+    static int population;
+    static int totalManges;
+    static int reproductions;
+
+    int renardsManges = 0;
+    int eauBue = 0;
+
+    void boire() {
+        if (Eau::boire(4)) eauBue += 4;
+    }
+
+    void manger(vector<Renard>& renards) {
+        if (renards.size() >= 2) {
+            renards.erase(renards.end() - 2, renards.end());
+            renardsManges += 2;
+            Renard::population -= 2;
+            Renard::totalManges += 2;
+        }
+    }
+
+    bool peutSeReproduire() const {
+        return renardsManges >= 30 && eauBue >= 30;
+    }
+
+    void resetReproduction() {
+        renardsManges = 0;
+        eauBue = 0;
+    }
+
+    static void reproduire(vector<Aigle>& aigles) {
+        for (size_t i = 0; i < aigles.size(); ++i) {
+            if (aigles[i].peutSeReproduire()) {
+                for (size_t j = i + 1; j < aigles.size(); ++j) {
+                    if (aigles[j].peutSeReproduire()) {
+                        aigles[i].resetReproduction();
+                        aigles[j].resetReproduction();
+                        aigles.push_back(Aigle());
+                        ++population;
+                        ++reproductions;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+};
+int Aigle::population = 2;
+int Aigle::totalManges = 0;
+int Aigle::reproductions = 0;
+
+void afficherStats(int tour) {
+    cout << "Tour " << tour + 1 << ":\n";
+    cout << "Aigles: " << Aigle::population << ", Renards: " << Renard::population
+        << ", Lapins: " << Lapin::population << "\n";
+    cout << "Total Aigles manges: " << Aigle::totalManges << ", Reproductions: " << Aigle::reproductions << "\n";
+    cout << "Total Renards manges: " << Renard::totalManges << ", Reproductions: " << Renard::reproductions << "\n";
+    cout << "Total Lapins manges: " << Lapin::totalManges << ", Reproductions: " << Lapin::reproductions << "\n";
+    cout << "Quantite d'eau: " << Eau::quantite << ", Quantite de trefles: " << Trefle::quantite << "\n\n";
+}
 
 int main() {
-    srand(static_cast<unsigned>(time(0))); // Initialise le générateur de nombres aléatoires
+    vector<Aigle> aigles(Aigle::population);
+    vector<Renard> renards(Renard::population);
+    vector<Lapin> lapins(Lapin::population);
 
-    Jeu jeu;
-    jeu.demarrer();
+    const int toursMax = 15;
+    for (int tour = 0; tour < toursMax; ++tour) {
+        // Actions des lapins
+        for (auto& lapin : lapins) {
+            lapin.boire();
+            lapin.manger();
+        }
+        Lapin::reproduire(lapins);
 
+        // Actions des renards
+        for (auto& renard : renards) {
+            renard.boire();
+            renard.manger(lapins);
+        }
+        Renard::reproduire(renards);
+
+        // Actions des aigles
+        for (auto& aigle : aigles) {
+            aigle.boire();
+            aigle.manger(renards);
+        }
+        Aigle::reproduire(aigles);
+
+        // Production des ressources (eau et trefles)
+        Eau::produire();
+        Trefle::produire();
+
+        // Affichage des statistiques pour ce tour
+        afficherStats(tour);
+    }
+
+    cout << "Simulation terminee apres " << toursMax << " tours.\n";
     return 0;
 }
+
+
