878e82fac2a6018f9f06f25e5ead9c473ffe4f26
Lord0k <lanfeust91@gmail.com>
2024-11-15 09:26:10 +0100

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -355,7 +355,7 @@ int main() {
 
     srand(time(0));
 
-    cout << "Yokoso watashi no soul society !\n\n";
+    cout << "YOKOSO WATASHI NO SOUL SOCIETY !\n\n";
 
     cout << "Aigles: " << Aigle::population << "\n";
     cout << "Nb de renards chasses: " << Aigle::totalManges << "\n";
